f7305c3ce860d2465ed2cd49e2d7be5e65a8ed16
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2026-04-19 21:04:37 +0100

    Update treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,29 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdate", "TSInstall", "TSInstallInfo" },
-    main = "nvim-treesitter.configs",
-    opts = {
-      ensure_installed = {
+    config = function()
+      require("nvim-treesitter").setup()
+
+      local parsers = {
         "bash", "c", "cpp", "diff", "dockerfile", "git_config", "gitcommit",
         "go", "gomod", "gosum", "gowork", "java", "json", "kotlin", "ledger",
         "lua", "make", "markdown", "markdown_inline", "python", "rust",
         "toml", "vim", "vimdoc", "yaml",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
+      }
+      require("nvim-treesitter").install(parsers)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local ft = vim.bo[args.buf].filetype
+          local lang = vim.treesitter.language.get_lang(ft)
+          if lang and pcall(vim.treesitter.start, args.buf, lang) then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
   },
 }
